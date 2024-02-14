d2cf2870c996b88f9087c9bed2acc9cf90077685
jbrugger <wwjbrugger@gmail.com>
2024-02-14 16:48:52 +0100

    delete white tabs from string representation of division

--- src/equation_classes/math_class/division.py
+++ src/equation_classes/math_class/division.py
@@ -17,20 +17,17 @@ class Division(AbstractOperator):
 
         elif call_node_id == self.node.parent_node.node_id or call_node_id is None:
             return f""" / {self.node.list_children[0].math_class
-            .prefix_notation(self.node.node_id, kwargs)} \
-            {self.node.list_children[1].math_class
+            .prefix_notation(self.node.node_id, kwargs)} {self.node.list_children[1].math_class
             .prefix_notation(self.node.node_id, kwargs)} """
 
         elif call_node_id == self.node.list_children[0].node_id:
             return f""" * {self.node.parent_node.math_class
-            .prefix_notation(self.node.node_id, kwargs)}' \
-            {self.node.list_children[1].math_class
+            .prefix_notation(self.node.node_id, kwargs)} {self.node.list_children[1].math_class
             .prefix_notation(self.node.node_id, kwargs)} """
 
         elif call_node_id == self.node.list_children[1].node_id:
             return f""" / {self.node.list_children[0].math_class
-            .prefix_notation(self.node.node_id, kwargs)} \
-            {self.node.parent_node.math_class
+            .prefix_notation(self.node.node_id, kwargs)} {self.node.parent_node.math_class
             .prefix_notation(self.node.node_id, kwargs)}"""
 
     def infix_notation(self, call_node_id, kwargs):
